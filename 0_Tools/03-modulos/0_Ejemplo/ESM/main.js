3e0fc6fc9e34936ad9190d17a44c8785c4e7f4b0
michel-lopez-franco <michel.lopez@academicos.udg.mx>
2026-02-06 10:27:22 -0600

    feat: agregar ejemplos de módulos ESM y CommonJS con funciones de suma

---
+++ 0_Tools/03-modulos/0_Ejemplo/ESM/main.js
@@ -0,0 +1,5 @@
+import suma from "./suma.js";
+
+console.log("Hello, World!");
+
+console.log("La suma de 2 y 3 es: " + suma(2, 3));
---
+++ 0_Tools/03-modulos/0_Ejemplo/commonjs/main.js
@@ -0,0 +1,7 @@
+const sumar = require("./suma");
+
+console.log("Hello, World!");
+
+
+console.log("La suma de 2 y 3 es: " + sumar(2, 3));
+
